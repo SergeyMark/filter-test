230ef6426a84fd8c5363385c985bcf2f9a53bafa
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-17 18:13:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ const tech = [
 
 const ref = {
     input: document.querySelector('#filter'),
-    list: document.querySelector('js-list-tech')
+    list: document.querySelector('.js-list-tech')
 }
 
+function createdListFn(){
+    return tech.map((item) => `<li>${item.label}</li>`).join('');
+}
 
+const createdList = createdListFn();
+ref.list.innerHTML = createdList;
+
+
+ref.input.addEventListener("input", inSearchInput);
+function inSearchInput(event) {
+    const filter = event.target.value;
+}
+
+function copyTechFilter() {
+    
+}
 
