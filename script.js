d71be6a033d2887e163a6098ecbbc359d05e1f90
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-17 13:44:18 +0200

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+const tech = [
+    {label: "HTML"},
+    {label: "CSS"},
+    {label: "JAVASCRIPT"},
+    {label: "PHP"},
+    {label: "JAVA"},
+    {label: "PYTHON"},
+    {label: "C++"},
+    {label: "C#"},
+    {label: "MYSQL"},
+    {label: "WORDPRESS"},
+    {label: "JOOMLA"}
+];
+
+const ref = {
+    input: document.querySelector('#filter'),
+    list: document.querySelector('js-list-tech')
+}
+
+
+
